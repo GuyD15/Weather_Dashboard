5de112f000ac34fc12b9a140153b9c376f93f59e
GuyD15 <guywayne.domino7@gmail.com>
2023-06-20 20:15:02 -0500

    Added variables for JS file, started search input information

---
+++ script.js
@@ -0,0 +1,14 @@
+var form = document.getElementById('search-bar');
+var searchInput = document.getElementById('search-input');
+var currentWeather = document.getElementById('current-weather');
+var forecast = document.getElementById('forecast');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    var cityName = searchInput.value;
+
+    if (cityName) {
+        
+    }
+})
